7f987eb00ef53b975ca0ba8656c0b090038aeaed
Keran <kerbraich@gmail.com>
2018-01-10 16:34:35 +0000

    add 20 character restriction to the createHtmlString function

--- spec/note-list-view-spec.js
+++ spec/note-list-view-spec.js
@@ -1,11 +1,11 @@
 function testCreateHtmlString() {
-  var note = new Note('Pesto rules');
-  var note2 = new Note('Only with pasta');
   var noteList = new NoteList();
+
+  noteList.createNote('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');
+  noteList.createNote('Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.');
+
   var noteListView = new NoteListView(noteList);
 
-  noteList.addNote(note)
-  noteList.addNote(note2)
-  assert.isTrue(noteListView.createHtmlString() === "<ul><li>Pesto rules</li><li>Only with pasta</li></ul>");
+  assert.isTrue(noteListView.createHtmlString() === "<ul><li>Lorem ipsum dolor si</li><li>Ut enim ad minim ven</li></ul>");
 };
 testCreateHtmlString();
--- views/note-list-view.js
+++ views/note-list-view.js
@@ -1,12 +1,13 @@
 (function(exports) {
 function NoteListView(noteList) {
+
   this.noteList = noteList;
 };
 
 NoteListView.prototype.createHtmlString = function() {
   return "<ul><li>" + this.noteList
   .getNotes()
-  .map(note => note.getNoteText())
+  .map(note => note.getNoteText().substr(0, 20))
   .join("</li><li>") + "</li></ul>";
 };
 
